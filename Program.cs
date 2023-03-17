b808db707a867bdcd3e5742fe248c02576bf5dd7
Peter <peterson.montez9@gmail.com>
2023-03-17 11:42:08 -0300

    abstracao

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+for (int i = 0; i < 6; i++)
+{
+    byte[] cores = new byte[9];
+    for (int j = 0; j < 9; j++)
+    {
+        cores[j] = 1;
+    }
+    //Face facezinha = new Face();
+} 
---
+++ face.cs
@@ -0,0 +1,8 @@
+public class Face{
+    public byte[] values { get; set; }
+
+    public Face(byte[] cores)
+    {
+        this.values = cores;
+    }
+}
