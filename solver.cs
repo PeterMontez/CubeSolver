0e26edc63a3fc8d114b6003df2e910db629eb7bb
Peter <peterson.montez9@gmail.com>
2023-04-14 00:33:48 -0300

    cross right orientation wrong place solve case added

--- solver.cs
+++ solver.cs
@@ -26,52 +26,74 @@ public class Solver
         
         while(!crossCheck())
         {
-            // ESSE FOR TESTA AS PEÇAS DA CRUZ QUE JÁ ESTÃO NA CRUZ, MAS NO LUGAR ERRADO, E COLOCA NO LUGAR CERTO. COD0x000
-            for (int i = 1; i < 8; i = i+2)
+            for (int r = 0; r < 4; r++)
             {
-                if (cubinho.cubo[5].values[i] == corBase && cubinho.cubo[(i-1)/2].values[7] != cubinho.cubo[(i-1)/2].values[4])
+                // ESSE FOR TESTA AS PEÇAS DA CRUZ QUE JÁ ESTÃO NA CRUZ, MAS NO LUGAR ERRADO, E COLOCA NO LUGAR CERTO. COD0x000
+                for (int k = 1; k < 8; k = k+2)
                 {
-                    cubinho.rotate((i-1)/2,true); 
-                    cubinho.rotate((i-1)/2,true); 
-                    crossAlg += " " + faceNome[(i-1)/2] + "2";
-                    for (int j = 0; j < 4; j++)
+                    int i;
+                    switch (k)
                     {
-                        if(cubinho.cubo[j].values[4] == cubinho.cubo[(i-1)/2].values[1])
+                        case 1:
+                            i = 0;
+                            break;
+                        case 5:
+                            i = 1;
+                            break;
+                        case 7:
+                            i = 2;
+                            break;
+                        default:
+                            i = k;
+                            break;
+                    }
+                    if (cubinho.cubo[5].values[k] == corBase && cubinho.cubo[i].values[7] != cubinho.cubo[i].values[4])
+                    {
+                        cubinho.rotate(i,true); 
+                        cubinho.rotate(i,true); 
+                        crossAlg += " " + faceNome[i] + "2";
+                        for (int j = 0; j < 4; j++)
                         {
-                            switch ((j-((i-1)/2)))
+                            if(cubinho.cubo[j].values[4] == cubinho.cubo[i].values[1])
                             {
-                                case 2: case -2:
-                                    cubinho.rotate(4,true);
-                                    cubinho.rotate(4,true);
-                                    crossAlg += " U2";
-                                    cubinho.rotate(j, true);
-                                    cubinho.rotate(j, true);
-                                    crossAlg += " " + faceNome[j] + "2";
-                                    break;
-                                case 1: case -3:
-                                    cubinho.rotate(4, false);
-                                    crossAlg += " U'";
-                                    cubinho.rotate(j, true);
-                                    cubinho.rotate(j, true);
-                                    crossAlg += " " + faceNome[j] + "2";
-                                    break;
-                                case -1: case 3:
-                                    cubinho.rotate(4, true);
-                                    crossAlg += " U";
-                                    cubinho.rotate(j, true);
-                                    cubinho.rotate(j, true);
-                                    crossAlg += " " + faceNome[j] + "2";
-                                    break;
-                                default:
-                                    Console.WriteLine("DEU UM ERRO AQUI, FORMAÇÃO DA CRUZ, PEÇA ORIENTADA CERTA NO LUGAR ERRADO");
-                                    break;
+                                switch ((j-(i)))
+                                {
+                                    case 2:
+                                    case -2:
+                                        cubinho.rotate(4,true);
+                                        cubinho.rotate(4,true);
+                                        crossAlg += " U2";
+                                        cubinho.rotate(j, true);
+                                        cubinho.rotate(j, true);
+                                        crossAlg += " " + faceNome[j] + "2";
+                                        break;
+                                    case 1:
+                                    case -3:
+                                        cubinho.rotate(4, false);
+                                        crossAlg += " U'";
+                                        cubinho.rotate(j, true);
+                                        cubinho.rotate(j, true);
+                                        crossAlg += " " + faceNome[j] + "2";
+                                        break;
+                                    case -1:
+                                    case 3:
+                                        cubinho.rotate(4, true);
+                                        crossAlg += " U";
+                                        cubinho.rotate(j, true);
+                                        cubinho.rotate(j, true);
+                                        crossAlg += " " + faceNome[j] + "2";
+                                        break;
+                                    default:
+                                        Console.WriteLine("DEU UM ERRO AQUI, FORMAÇÃO DA CRUZ, PEÇA ORIENTADA CERTA NO LUGAR ERRADO");
+                                        break;
+                                }
+                                break;
                             }
                         }
                     }
                 }
-
+                //FIM DO FOR COD0x000
             }
-            //FIM DO FOR COD0x000
             Console.WriteLine(crossAlg);
 
 
